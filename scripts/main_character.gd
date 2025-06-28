3e26f9d9d0508600e36d07c14e0b4fcfcb3be447
Nourcova <ibrahamnour22@gmail.com>
2025-06-28 20:53:27 +0300

    level 1 finished

--- scripts/main_character.gd
+++ scripts/main_character.gd
@@ -4,10 +4,14 @@ extends CharacterBody2D
 const SPEED = 400.0
 const JUMP_VELOCITY = -900.0
 @onready var sprite_2d = $Sprite2D
+@export var particle: PackedScene
 var is_shrunk = false
+
+var jump_count = 0
 
 func jump():
 	velocity.y = JUMP_VELOCITY
+	spawn_particles()
 	
 func jump_side(x):
 	velocity.y = JUMP_VELOCITY
@@ -24,21 +28,27 @@ func _physics_process(delta):
 			global_position.y -= 16  # adjust this value to match your character's height difference
 			scale = Vector2(1, 1)
 			is_shrunk = false
-	#Animations
-	if abs(velocity.x) > 1:
-		sprite_2d.animation = "running"
-	else:
-		sprite_2d.animation = "default"
 		
 	# Add the gravity.
-	if not is_on_floor():
+	if is_on_floor():
+		jump_count = 0
+		#Animations
+		if abs(velocity.x) > 1:
+			sprite_2d.animation = "running"
+		else:
+			sprite_2d.animation = "default"
+	else:
 		velocity += get_gravity() * delta
-		sprite_2d.animation = "jumping"
-
+		if(jump_count == 2):
+			sprite_2d.animation = "double_jumping"
+		else:
+			sprite_2d.animation = "jumping"
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and jump_count < 2:
 		velocity.y = JUMP_VELOCITY
-
+		jump_count += 1
+		if jump_count == 2:
+			spawn_particles()
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
@@ -52,4 +62,10 @@ func _physics_process(delta):
 	var isLeft = velocity.x < 0
 	if velocity.x != 0:
 		sprite_2d.flip_h = velocity.x < 0
-		
+
+func spawn_particles():
+	var particle_node = particle.instantiate()
+	particle_node.position = position
+	get_parent().add_child(particle_node)
+	await get_tree().create_timer(0.3).timeout
+	particle_node.queue_free()
